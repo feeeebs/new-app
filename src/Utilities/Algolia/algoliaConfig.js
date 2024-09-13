2acc3e73538992ff1e07cc407add7c880a2e378c
Phoebe Mueller <phoebe.mueller1@gmail.com>
2024-09-12 21:04:09 -0500

    updating algolia app

--- src/Utilities/Algolia/algoliaConfig.js
+++ src/Utilities/Algolia/algoliaConfig.js
@@ -1,5 +1,6 @@
 import algoliasearch from "algoliasearch";
 
-const algoliaClient = algoliasearch("CP65BSPR37", "deadada9df27054aede4356c8d7e1e94");
+//const algoliaClient = algoliasearch("CP65BSPR37", "deadada9df27054aede4356c8d7e1e94");
+const algoliaClient = algoliasearch("Z597CJTEEG", "002b817cb89d54b8ddeeb8fb493b2b43");
 
 export default algoliaClient;
